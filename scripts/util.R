637330b2c6bfc0e98858bf716b3ce92e89651781
Jim Urick <jimurick@gmail.com>
2024-11-04 17:48:01 -0500

    small changes

--- scripts/util.R
+++ scripts/util.R
@@ -1,7 +1,7 @@
 GH_URL <- "https://github.com/jimurick/vermont"
 
 repo_url <- function(gh_path) {
-  stringr::str_c(GH_URL, "/blob/main/", gh_path)
+  stringr::str_c(GH_URL, "/tree/main/", gh_path)
 }
 
 repo_link <- function(label, repo_path) {
